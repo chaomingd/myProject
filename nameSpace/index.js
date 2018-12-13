8b4fb9710dcfa146b58c885bbacf329c3339239b
fpgdev <997773513@qq.com>
2018-12-13 15:52:01 +0800

    1213

---
+++ nameSpace/index.js
@@ -0,0 +1,13 @@
+var _nameSpace = {com: {greensock: {test: 'test'}}};
+function nameSpace(ns) {
+	var a = ns.split('.');
+	var p = _nameSpace || {};
+	for(i = 0;i < a.length; i ++) {
+		p[a[i]] = p[a[i]] || {}
+		p = p[a[i]];
+	}
+	return p;
+}
+gs = nameSpace('com.greensock')
+console.log(gs)
+
